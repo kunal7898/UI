41b7e69f010bb2ec0291c425ca58f5899c6c8972
Kunal Singh <kunal.singh@excellonsoft.com>
2018-07-17 16:59:33 +0530

    Changes

--- src/app/AppCommon/App.Constant.ts
+++ src/app/AppCommon/App.Constant.ts
@@ -2,4 +2,4 @@ export const AppSettings = Object.freeze({
 BASE_URL:"http://dvpvmazure.eastus.cloudapp.azure.com/UPAY",
 LOGIN_URL:"http://dvpvmazure.eastus.cloudapp.azure.com/UPAY/Aauth/token",
 
-});
+}); 
